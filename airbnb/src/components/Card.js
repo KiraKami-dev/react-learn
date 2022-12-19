33477f3d2ddb5fa8ddefa54a866c2a9564cc8119
Kira <akshatkhair33@gmail.com>
2022-12-19 09:50:49 +0530

    Airbnb with mapping

--- airbnb/src/components/Card.js
+++ airbnb/src/components/Card.js
@@ -1,23 +1,26 @@
 import React from "react";
-import Star from "../img/Star.png"
-import circle from "../img/circle.png"
+import Star from "../img/Star.png";
+import circle from "../img/circle.png";
 
-export default function Card(props){
-    return(
-        <div className="container">
-            <div className="subcont1">
-                <img src={props.img} className='cardimg'/>
-                <div className="sold">{props.status}</div>
-            </div>
-            <div className="starrow">
-                <img src={Star} className="starimg"/>
-                <p className="rating">{props.rating}</p>
-                <p className="grey">({props.review})</p>
-                <img src={circle}/>
-                <p className="grey">{props.contry}</p>
-            </div>
-            <p className="content--title">{props.title}</p>
-            <p className="content--price"><b>From ${props.price} </b>/ person</p>
-        </div>
-    )
+
+export default function Card(props) {
+  return (
+    <div className="container">
+      <div className="subcont1">
+        <img src={props.img} className="cardimg" />
+        <div className="sold">{props.status}</div>
+      </div>
+      <div className="starrow">
+        <img src={Star} className="starimg" />
+        <p className="rating">{props.rating}</p>
+        <p className="grey">({props.review})</p>
+        <img src={circle} />
+        <p className="grey">{props.country}</p>
+      </div>
+      <p className="content--title">{props.title}</p>
+      <p className="content--price">
+        <b>From ${props.price} </b>/ person
+      </p>
+    </div>
+  );
 }
--- airbnb/src/index.js
+++ airbnb/src/index.js
@@ -4,44 +4,27 @@ import ReactDOM from "react-dom";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
-import card1 from "./img/card1.png";
-import card2 from "./img/card2.png";
-import card3 from "./img/card3.png";
+import data from "./data/data";
 
 function App() {
+  const dataelement = data.map((items) => {
+    return (
+      <Card
+        img={items.coverImg}
+        status={items.location}
+        rating={items.stats.rating}
+        review={items.stats.reviewCount}
+        country="USA"
+        title={items.title}
+        price={items.price}
+      />
+    );
+  });
   return (
     <>
       <Navbar />
       <Hero />
-      <div className="appcont">
-        <Card
-          img={card1}
-          status="SOLD OUT"
-          rating="5.0"
-          review="6"
-          contry="USA"
-          title="Life lessons with Katie Zaferes"
-          price="136"
-        />
-        <Card
-          img={card2}
-          status="ONLINE"
-          rating="4.5"
-          review="10"
-          contry="Italy"
-          title="Learn wedding photography"
-          price="125"
-        />
-        <Card
-          img={card3}
-          status="ONLINE"
-          rating="4.8"
-          review="8"
-          contry="Italy"
-          title="Group Mountain Biking"
-          price="50"
-        />
-      </div>
+      <div className="appcont">{dataelement}</div>
     </>
   );
 }
